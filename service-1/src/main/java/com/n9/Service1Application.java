d4fe52a87fd9c2e8b39bbac563b7aa57bebe1cea
nityaay8 <nityaay8@gmail.com>
2022-08-24 13:32:33 +0530

    circuit breaker changes

---
+++ service-1/src/main/java/com/n9/Service1Application.java
@@ -0,0 +1,12 @@
+package com.n9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+public class Service1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Service1Application.class);
+    }
+}
